1c8cb40df6025c7a311044ad65a14e92fbff7e23
Tommy <tommy.grabowski@revature.net>
2021-11-03 12:07:14 -0400

    Add CustomerRepository interface

---
+++ src/main/java/com/revature/p2BackEnd/repo/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.revature.p2BackEnd.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.p2BackEnd.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	public List<Customer> findByEmail(String email);
+
+	public List<Customer> findByMobile(String mobile);
+}
