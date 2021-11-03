09a1beef38cddb94d498bb07b742c44b6ef83e0d
Tommy <tommy.grabowski@revature.net>
2021-11-03 12:03:32 -0400

    Add to Customer class

--- src/main/java/com/revature/p2BackEnd/model/Customer.java
+++ src/main/java/com/revature/p2BackEnd/model/Customer.java
@@ -7,23 +7,29 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "customer")
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Customer {
-		@Id
-		@GeneratedValue
-		private int id;
-		private String name;
-		private String email;
-		private String mobile;
-		private String password;
-		@Column(name = "is_active")
-		private boolean isActive;
-		
-		
-		
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String email;
+	private String mobile;
+	private String password;
+	@Column(name = "is_active")
+	private boolean isActive;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
